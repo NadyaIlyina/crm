6508f8dd5df347cf826bdd96b3de477eb3e5f280
Nadya Ilina <nadya_ilina_1998@mail.ru>
2020-11-14 18:21:58 +0300

    add all files

---
+++ src/test/java/com/crm/carmen/CarmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.carmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarmenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
